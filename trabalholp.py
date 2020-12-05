1f767815294a1191936bb765df0d0a50feac6f03
Cassiano de Souza <71332818+cassianodess@users.noreply.github.com>
2020-12-05 14:24:19 -0300

    Update trabalholp.py

--- trabalholp.py
+++ trabalholp.py
@@ -7,41 +7,53 @@ Trabalho 3 LP
 class MinhaData:
 
     def __init__(self, dia, mes, ano):
-        self.dia = dia
-        self.mes = mes
-        self.ano = ano
+        self.__dia = dia
+        self.__mes = mes
+        self.__ano = ano
+
+    @property
+    def dia(self):
+        return self.__dia
+
+    @property
+    def mes(self):
+        return self.__mes
+
+    @property
+    def ano(self):
+        return self.__ano
 
     # Criar um construtor que receba uma string e inicialize a data
     def __init__(self, string):
         lista = string.split("/")
-        self.dia = int(lista[0])
-        self.mes = int(lista[1])
-        self.ano = int(lista[2])
+        self.__dia = int(lista[0])
+        self.__mes = int(lista[1])
+        self.__ano = int(lista[2])
 
     # Não entendi muito bem o que pediu
     def ToString(self):
         lista = []
-        lista.append(self.dia)
-        lista.append(self.mes)
-        lista.append(self.ano)
+        lista.append(self.__dia)
+        lista.append(self.__mes)
+        lista.append(self.__ano)
         string = ''.join(lista)
         return string
 
     def compara(self, data):
-        if (self.dia == data.dia) and (self.mes == data.mes) and (self.ano == data.ano):
+        if (self.__dia == data.dia) and (self.__mes == data.mes) and (self.__ano == data.ano):
             return 0
 
-        if (self.ano == data.ano) and (self.mes == data.mes):
-            if (self.dia > data.dia):
+        if (self.__ano == data.ano) and (self.__mes == data.mes):
+            if (self.__dia > data.__dia):
                 return 1
             else:
                 return -1
-        if (self.ano == data.ano):
-            if (self.mes > data.mes):
+        if (self.__ano == data.ano):
+            if (self.__mes > data.mes):
                 return 1
             else:
                 return -1
-        if (self.ano > data.ano):
+        if (self.__ano > data.ano):
             return 1
         else:
             return -1
@@ -54,39 +66,59 @@ class DataComemorativa:
     day = ""
 
     def __init__(self, nome, feriado, Fmundial, day):
-        self.nome = nome
-        self.feriado = feriado
-        self.Fmundial = Fmundial
-        self.day = day
-        # print(f"{self.nome}, {self.feriado}, {self.Fmundial}, {self.day}")
+        self.__nome = nome
+        self.__feriado = feriado
+        self.__Fmundial = Fmundial
+        self.__day = day
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def feriado(self):
+        return self.__feriado
+
+    @property
+    def Fmundial(self):
+        return self.__Fmundial
+
+    @property
+    def day(self):
+        return self.__day
 
 
 class DatasComemorativas:
     def __init__(self):
-        self.datas = []
+        self.__datas = []
+
+    @property
+    def datas(self):
+        return self.__datas
 
     def adiciona_data(self, data):
-        self.datas.append(data)
+        self.__datas.append(data)
 
     def lista_datas(self):
-        for data in self.datas:
-            print(data.nome, data.feriado, data.Fmundial, data.day)
+        for data in self.__datas:
+            print(data.__nome, data.__feriado, data.__Fmundial, data.__day)
 
     def remove_data(self, nome):
-        for data in self.datas:
-            if nome in data.nome:
-                del data.nome
-                del data.day
-                del data.feriado
-                del data.Fmundial
+        for data in self.__datas:
+            if nome in data.__nome:
+                del data.__nome
+                del data.__day
+                del data.__feriado
+                del data.__Fmundial
 
     def horas_nao_trabalhadas(self):
         cont = 0
-        for data in self.datas:
+        for data in self.__datas:
             if (data.feriado == True):
-                cont+=1
-        print(f"{cont*8}h")
-        return cont*8
+                cont += 1
+        print(f"{cont * 8}h")
+        return cont * 8
+
 
 atual = MinhaData("05/12/2020")
 natal = MinhaData("25/12/2020")
@@ -95,4 +127,3 @@ natal = DataComemorativa("Natal", True, True, "25/12/2020")
 feriado = DatasComemorativas()
 feriado.adiciona_data(natal)
 feriado.horas_nao_trabalhadas()
-
